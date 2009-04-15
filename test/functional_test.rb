4c8f24c080689d33a4be820c79ac37761137ba3c
Jose M. Prieto <jmprieto@gmx.net>
2009-04-15 22:09:36 +0200

    add a license

--- test/functional_test.rb
+++ test/functional_test.rb
@@ -1,5 +1,9 @@
 #!/usr/bin/ruby
 
+# José M. Prieto <jmprieto@gmx.net> wrote this file. As long as you retain
+# this notice you can do whatever you want with this stuff. If we meet some
+# day, and you think this stuff is worth it, you can buy me a beer in return.
+
 require 'test/unit'
 require File.dirname(__FILE__) + '/../redmine'
 
